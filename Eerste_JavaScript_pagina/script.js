d4cfab4c1f779a91ffa9df1b674806ff85164793
limonije <mroozen@live.nl>
2021-06-28 16:23:25 +0200

    Add files via upload

---
+++ Eerste_JavaScript_pagina/script.js
@@ -0,0 +1,19 @@
+console.log('Hello Winc Academy');
+let naam='Monique';
+console.log(naam);
+let number1=4;
+let number2=4;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+let string1="4";
+let string2="4";
+console.log(string1+string2);
+console.log(string1-string2);
+console.log(string1*string2);
+console.log(string1/string2);
+console.log(string1%string2);
+let leeftijd="50";
+console.log(typeof leeftijd)
